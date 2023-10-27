d94016e80310f613cc0e9a5fd1125edb5c231be2
Dogeydogey <149156029+Dogeydogey@users.noreply.github.com>
2023-10-27 10:57:51 +0100

    Update index.tsx

--- src/games/Mines/index.tsx
+++ src/games/Mines/index.tsx
@@ -5,7 +5,7 @@ export default {
   name: 'Mines',
   short_name: 'mines',
   description: `
-    There's money hidden beneath the squares. The reward will increase the more squares you reveal, but watch out for the 5 hidden mines. Touch one and you'll go broke. You can cash out at any time.
+    creator :ACoPzg7tNqhBXJm4JSQwKrJ7LiocezBTSZRQfLmrxpZz
   `,
   theme_color: 'rgb(138 102 255)',
   image,
